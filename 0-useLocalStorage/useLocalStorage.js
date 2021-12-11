04f7e707c10bf3361f03783f6ce6ed7d6c985f26
Taro Serigano <taroserigano@gmail.com>
2021-12-11 17:00:49 -0600

    Update useLocalStorage.js

--- 0-useLocalStorage/useLocalStorage.js
+++ 0-useLocalStorage/useLocalStorage.js
@@ -14,7 +14,7 @@ export default function useLocalStorage(key, initialValue){
   })
   useEffect(() =>  { 
     localStorage.setItem(key, JSON.stringify(value))
-  }, [key, value]) 
+  }, [value]) 
 
   return [value, setValue]
 }
